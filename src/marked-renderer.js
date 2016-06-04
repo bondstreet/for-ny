d4589d82dc8e2fe977d460035ec510fa9d9d1d35
jxnblk <jacksonblack@gmail.com>
2016-06-04 14:06:36 -0400

    Move data file

--- src/marked-renderer.js
+++ src/marked-renderer.js
@@ -3,11 +3,6 @@ const marked = require('marked')
 
 const renderer = new marked.Renderer()
 
-// renderer.image = (href, title, text) => {
-//     console.log('image render', href, title, text)
-//     return `<img src='${href}' />`
-// }
-
 renderer.paragraph = (text) => {
     if (/^\<img/.test(text)) {
         return text
