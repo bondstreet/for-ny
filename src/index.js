1573d222e12e2213c78379904112f6d7a6c2868e
anh-thu huynh <oldwestaction@users.noreply.github.com>
2016-06-06 11:17:38 -0400

    Pass current post data to renderHtml (#14)
    * Pass current post data to renderHtml
    
    * Remove console log

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ReactDOMServer from 'react-dom/server'
 import { match } from 'react-router'
+import { find } from 'lodash'
 import routes from './routes'
 import renderHtml from './renderHtml'
 import Root from './components/Root'
@@ -13,17 +14,30 @@ if (typeof document !== 'undefined') {
 	ReactDOM.render(<Root data={initialData} />, div)
 }
 
+const getCurrentPost = (posts, path) => {
+    if (!/^\/posts\//.test(path)) {
+        return false
+    }
+    const id = path.replace(/^\/posts\/|index\.html$/, '')
+    if (id) {
+        const post = find(posts, p => p.name === id)
+        return post
+    }
+}
+
 const render = ({
 	webpackStats,
 	assets,
 	...props
 }, callback) => {
+    const post = getCurrentPost(props.posts, props.path)
+
 	match({ routes, location: props.baseurl + props.path }, (err, redirectLocation, renderProps) => {
 		const app = ReactDOMServer.renderToString(
             <Root data={props} renderProps={renderProps} />
 		)
 
-		const html = renderHtml({ app, ...props })
+		const html = renderHtml({ app, post, ...props })
 
 		callback(null, `<!DOCTYPE html>${html}`)
 	})
--- src/renderHtml.js
+++ src/renderHtml.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'development') {
     script = '/dev.js'
 }
 
-const Head = ({ title }) => {
+const Head = ({ title, post }) => {
     return (
         <head>
             <meta charSet='utf-8' />
