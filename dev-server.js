7547ee9f91ef4445a60a6d3f838067e1649084f6
Forrest <fdscofield@gmail.com>
2016-06-06 10:04:23 -0400

    changed dev port to 8081

--- dev-server.js
+++ dev-server.js
@@ -8,7 +8,7 @@ new WebpackDevServer(webpack(config), {
     hot: true,
     historyApiFallback: true,
     contentBase: '/dist',
-}).listen(8080, 'localhost', function (err, result) {
+}).listen(8081, 'localhost', function (err, result) {
     if (err) {
         return console.log(err)
     }
