2674b866300baa52e94e4ed2440820d38ddaed09
Anh-Thu Huynh <anh-thu@bondstreet.com>
2016-06-06 12:36:45 -0400

    adding opengraph metadata for the landing page + profile pages

--- src/renderHtml.js
+++ src/renderHtml.js
@@ -11,18 +11,27 @@ if (process.env.NODE_ENV === 'development') {
     script = '/dev.js'
 }
 
-const Head = ({ title, metadata, post }) => {
+const Head = ({ title, metadata, post, ...props }) => {
+    const ogTitle = post.title || metadata.title
+    const ogImage = post.image || metadata.image
+    const ogUrl = props.domain + props.baseurl + props.path
+    const ogDescription = post.blurb || metadata.description
+
     return (
         <head>
             <meta charSet='utf-8' />
             <title>{title}</title>
-            {/* OG meta tags etc. goes here */}
+            <meta property='og:title' content={ogTitle} />
+            <meta property='og:type' content='website' />
+            <meta property='og:url' content={ogUrl} />
+            <meta property='og:image' content={ogImage} />
+            <meta property='og:description' content={ogDescription} />
             <style dangerouslySetInnerHTML={{ __html: css }}/>
             <script src='https://use.typekit.net/zzi1igz.js' />
             <script dangerouslySetInnerHTML={{
                 __html: 'try{Typekit.load({ async: true });}catch(e){}'
             }} />
-    </head>
+        </head>
     )
 }
 
