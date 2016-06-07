64f23fe21e874eff92933a150e240a7f0655af71
Forrest <fdscofield@gmail.com>
2016-06-07 11:49:13 -0400

    moved modal logic to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,62 @@
 
 import React from 'react'
 import rebassConfig from './rebass-config'
+import LeadModal from './LeadModal'
+import { initInactiveTrigger, initTimeoutTrigger, modalSeen, setModalSeenCookie } from '../modal-triggers'
 
 class App extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            modalOpen: false
+        }
+
+        this.closeModal = this.closeModal.bind(this)
+        this.launchModal = this.launchModal.bind(this)
+    }
+
+
     getChildContext () {
         return {
-            rebass: rebassConfig
+            rebass: rebassConfig,
+            modal: {
+                open: this.state.modalOpen,
+                closeModal: this.closeModal
+            }
         }
+    }
+
+    componentDidMount() {
+        initInactiveTrigger(this.launchModal)
+        initTimeoutTrigger(this.launchModal)
+    }
+
+    launchModal () {
+        if (!modalSeen()) {
+            this.setState({modalOpen: true})
+            setModalSeenCookie()
+        }
+    }
+
+    closeModal () {
+        this.setState({modalOpen: false})
     }
 
     render () {
         return (
             <div>
                 {this.props.children}
+                <LeadModal />
             </div>
+
         )
     }
 }
 
 App.childContextTypes = {
-    rebass: React.PropTypes.object
+    rebass: React.PropTypes.object,
+    modal: React.PropTypes.object
 }
 
 App.contextTypes = {
@@ -27,4 +64,3 @@ App.contextTypes = {
 }
 
 export default App
-
--- src/components/Index.js
+++ src/components/Index.js
@@ -9,7 +9,6 @@ import LandingPosts from './LandingPosts'
 import SocialPromo from './SocialPromo'
 import EventList from './EventList'
 import LandingAbout from './LandingAbout'
-import LeadModal from './LeadModal'
 import Footer from './Footer'
 
 const Index = (props, { data }) => {
@@ -24,7 +23,6 @@ const Index = (props, { data }) => {
       <LandingAbout />
       <LeadForm />
       <Footer />
-      <LeadModal />
     </div>
   )
 }
