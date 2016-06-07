225ef6d7b8d6f92678cbcbab7f809f2965a1a977
jxnblk <jacksonblack@gmail.com>
2016-06-07 12:50:26 -0400

    Update events

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -3,16 +3,18 @@ import React from 'react'
 
 const EventCard = ({ ...props }) => {
     return (
-        <div className='md-flex items-start'>
+        <div className='md-flex items-start items-center'>
             <div className='h0 mr4 bold'>{props.day}</div>
             <div className='flex-auto'>
                 <span className='h3 bold caps'>{props.title}</span><br />
-                {props.location}<br />
-                {props.blurb}<br />
+                <div>{props.location}</div>
+                {props.blurb && <div>{props.blurb}</div>}
             </div>
-            <div>
-                <a href={props.link} className='h3 bold color-inherit caps'>RSVP</a>
-            </div>
+            {props.link && (
+                <div>
+                    <a href={props.link} className='h3 bold color-inherit caps'>RSVP</a>
+                </div>
+            )}
         </div>
     )
 }
