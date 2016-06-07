80c70b104282e5486af826784be90a683ff9a937
Forrest <fdscofield@gmail.com>
2016-06-07 16:21:59 -0400

    Remove duplicated heading

--- src/components/CheckOrSchedule.js
+++ src/components/CheckOrSchedule.js
@@ -6,23 +6,18 @@ import { ButtonOutline } from 'rebass'
 export const CheckOrSchedule = ({onNotReadyClick, onAccept, ...props}) => {
     return (
         <div>
-            <h2>
-                Ready to grow your business? Find out of Bond Street is right for you
-            </h2>
-            <div>
-                <ButtonOutline
-                    id='qlm-1-not-interested'
-                    m={1}
-                    href='/apply/'>
-                    Check my rate
-                </ButtonOutline>
-                <ButtonOutline
-                    id='qlm-1-interested'
-                    m={1}
-                    onClick={onAccept}>
-                    Schedule a free consultation
-                </ButtonOutline>
-            </div>
+            <ButtonOutline
+                id='qlm-1-not-interested'
+                m={1}
+                href='/apply/'>
+                Check my rate
+            </ButtonOutline>
+            <ButtonOutline
+                id='qlm-1-interested'
+                m={1}
+                onClick={onAccept}>
+                Schedule a free consultation
+            </ButtonOutline>
             <button onClick={onNotReadyClick}>No thanks, I’m not ready quite yet but would like to stay in touch</button>
         </div>
     )
