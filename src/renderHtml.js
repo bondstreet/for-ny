f6281c7d1f4bc0e24d3388791c684230b3326759
Forrest <fdscofield@gmail.com>
2016-06-07 17:07:50 -0400

    Added optimizely tag

--- src/renderHtml.js
+++ src/renderHtml.js
@@ -28,6 +28,7 @@ const Head = ({ title, metadata, post, ...props }) => {
             <meta property='og:image' content={ogImage} />
             <meta property='og:description' content={ogDescription} />
             <style dangerouslySetInnerHTML={{ __html: css }}/>
+            <script src='https://cdn.optimizely.com/js/6134185353.js'></script>
             <script src='https://use.typekit.net/zzi1igz.js' />
             <script dangerouslySetInnerHTML={{
                 __html: 'try{Typekit.load({ async: true });}catch(e){}'
