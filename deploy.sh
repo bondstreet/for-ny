14bb9c1774c1ea2056c244b0990006e2ec723d25
jxnblk <jacksonblack@gmail.com>
2016-06-07 17:21:52 -0400

    Adjust deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
 echo 'Deploying to gh-pages...'
-npm run clean || exit 0
-npm run build
-git init
 git config user.name "Travis-CI"
 git config user.email "travis@bondstreet.com"
+npm run clean || exit 0
+npm run build
 npm run travis:deploy
 
