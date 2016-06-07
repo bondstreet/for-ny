c724bf9f064858bc573b6a36db94357cfa0e33c4
Forrest <fdscofield@gmail.com>
2016-06-07 12:26:03 -0400

    added different segment names depending on button clicked

--- src/components/CheckOrSchedule.js
+++ src/components/CheckOrSchedule.js
@@ -2,24 +2,22 @@ import React from 'react'
 import { Button} from 'rebass'
 
 
-export const CheckOrSchedule = ({onDecline, onAccept, ...props}) => {
+export const CheckOrSchedule = ({onNotReadyClick, onAccept, ...props}) => {
     return (
         <div>
             <h2>
                 Ready to grow your business? Find out of Bond Street is right for you
             </h2>
             <div>
-                <Button outline
-                    id='qlm-1-not-interested'
-                    onClick={onDecline}>
+                <Button outline>
                     <a href='/apply/'>Check my rate</a>
                 </Button>
                 <Button
-                    id='qlm-1-interested'
                     onClick={onAccept}>
                     Schedule a free consultation
                 </Button>
             </div>
+            <a onClick={onNotReadyClick}>No thanks, I'm not ready quite yet but would like to stay in touch</a>
         </div>
     )
 }
--- src/components/LeadForm.js
+++ src/components/LeadForm.js
@@ -4,6 +4,8 @@ import CheckOrSchedule from './CheckOrSchedule'
 import LeadCapture from './LeadCapture'
 import Schedule from './Schedule'
 import LikeUs from './LikeUs'
+import { setModalSeenCookie } from '../modal-triggers'
+
 
 
 class LeadForm extends React.Component {
@@ -17,12 +19,15 @@ class LeadForm extends React.Component {
             business_name: '',
             name: '',
             email: '',
-            phone_number: ''
+            phone_number: '',
+            segment_name: ''
         }
 
         this.setView = this.setView.bind(this)
-        this.handleLeadSubmit = this.handleLeadSubmit.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.handleLeadSubmit = this.handleLeadSubmit.bind(this)
+        this.onReadyToSchedule = this.onReadyToSchedule.bind(this)
+        this.onNotReadyToSchedule = this.onNotReadyToSchedule.bind(this)
     }
 
     setView (view) {
@@ -39,6 +44,7 @@ class LeadForm extends React.Component {
     handleLeadSubmit (e) {
         e.preventDefault()
         const payload = {
+            segment_name: this.state.segment_name,
             company: this.state.business_name,
             full_name: this.state.name,
             email: this.state.email,
@@ -48,6 +54,21 @@ class LeadForm extends React.Component {
 
         this.createLead(payload).then(() => {
             this.setView('schedule')
+            setModalSeenCookie(365)
+        })
+    }
+
+    onReadyToSchedule () {
+        this.setState({
+            segment_name: 'fornyc__ready_to_schedule',
+            view: 'lead'
+        })
+    }
+
+    onNotReadyToSchedule () {
+        this.setState({
+            segment_name: 'fornyc__not_ready_to_schedule',
+            view: 'lead'
         })
     }
 
@@ -69,7 +90,8 @@ class LeadForm extends React.Component {
             ),
             checkOrSchedule: (
                 <CheckOrSchedule
-                    onAccept={this.setView('lead')}
+                    onNotReadyClick={this.onNotReadyToSchedule}
+                    onAccept={this.onReadyToSchedule}
                     {...this.state}/>
             ),
             lead: (
