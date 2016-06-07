642cfc74ae87dae3d677f0ddeec641b55b9949d3
jxnblk <jacksonblack@gmail.com>
2016-06-07 18:24:15 -0400

    Uncomment travis deploy conditional

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
 echo 'Deploying to gh-pages...'
 npm run clean || exit 0
 npm run build
