8491b03a569e0136aabf8200674ed9fe6fccb9be
Anh-Thu Huynh <anh-thu@bondstreet.com>
2016-06-08 17:24:46 -0400

    event tweaks

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,18 +1,21 @@
 
 import React from 'react'
+import moment from 'moment'
 import Heading from './Heading'
 import Text from './Text'
 
 const EventCard = ({ ...props }) => {
     return (
-        <div className='md-flex items-center'>
+        <div className='md-flex mb2 items-center' style={{ alignItems: 'flex-start' }}>
             <Heading size={0}
-                className='mr2'
+                className='mr2 center'
                 style={{
+                    border: '3px solid',
                     flexShrink: 0,
                     flexBasis: 128
                 }}>
-                {props.day}
+                <Text caps>{moment(props.month, 'MM').format('MMM')}</Text>
+                <span style={{ letterSpacing: 0 }}>{props.day}</span>
             </Heading>
             <div className='flex-auto'>
                 <Text caps>{props.title}</Text>
