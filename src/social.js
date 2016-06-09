25b8af90ff451794684d6abb7c46b0088168e661
Anh-Thu Huynh <anh-thu@bondstreet.com>
2016-06-09 16:32:34 -0400

    trying to fix async js errors

--- src/social.js
+++ src/social.js
@@ -1,11 +1,21 @@
+// https://developers.facebook.com/docs/javascript/quickstart
 // https://developers.facebook.com/docs/plugins/share-button/#configurator
-const facebook = `(function(d, s, id) {
+const facebook = `
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '158471404493763',
+            xfbml      : true,
+            version    : 'v2.6'
+        });
+    };
+    (function(d, s, id){
         var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
+        if (d.getElementById(id)) {return;}
         js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        // js.src = "//connect.facebook.net/en_US/sdk/debug.js";
         fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'))
+    }(document, 'script', 'facebook-jssdk'));
     `
 // https://about.twitter.com/resources/buttons#tweet
 const twitter = `!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');`
@@ -13,8 +23,12 @@ const twitter = `!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^h
 const socialScripts = facebook + twitter
 
 const initSocialScripts = function() {
-    FB.XFBML.parse();
-    twttr.widgets.load();
+    if (typeof FB !== 'undefined') {
+        FB.XFBML.parse(document.getElementById('app'))
+    }
+    if (typeof twittr !== 'undefined') {
+        twttr.widgets.load(document.getElementById('app'))
+    }
 }
 
 export {
