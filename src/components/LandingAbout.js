35c005d4ab5c292013958080f8480c2178666778
Forrest <fdscofield@gmail.com>
2016-06-14 14:15:34 -0400

    remove console log

--- src/components/LandingAbout.js
+++ src/components/LandingAbout.js
@@ -15,9 +15,7 @@ const LandingAbout = (props, { data }) => {
         aboutButton,
         logos
     } = data.landing.about
-
-    console.log(logos)
-
+    
     return (
         <Scroll.Element
             name='about'
