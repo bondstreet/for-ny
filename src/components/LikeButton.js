c9041d648c7698509e8bd926e6f746a8d04ae8c6
Anh-Thu Huynh <anh-thu@bondstreet.com>
2016-06-14 12:12:22 -0400

    reinit landing nav like button when returning to landing page

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,19 +1,43 @@
 
 import React from 'react'
 
-const LikeButton = (props, { data }) => {
-    return (
-        <div className='fb-like'
-            data-href='https://bondstreet.com/fornewyork'
-            data-layout='button_count'
-            data-action='like'
-            data-show-faces='false'
-            data-share='false'></div>
-    )
-}
+class LikeButton extends React.Component {
+    constructor () {
+        super()
+        this.refresh = this.refresh.bind(this)
+        this.createDiv = this.createDiv.bind(this)
+    }
 
-LikeButton.contextTypes = {
-    data: React.PropTypes.object
+    componentDidMount () {
+        this.createDiv()
+        this.refresh()
+    }
+
+    refresh () {
+        if (typeof FB !== 'undefined') {
+            FB.XFBML.parse()
+        }
+    }
+
+    createDiv () {
+        this.refs.root.innerHTML = null
+        const div = document.createElement('div')
+        div.className = 'fb-like'
+        div.setAttribute('data-href', 'https://bondstreet.com/fornewyork')
+        div.setAttribute('data-layout', 'button_count')
+        div.setAttribute('data-action', 'like')
+        div.setAttribute('data-show-faces', false)
+        div.setAttribute('data-share', false)
+        this.refs.root.appendChild(div)
+    }
+
+    render () {
+        const sx = {
+            display: 'inline-block'
+        }
+
+        return <div ref='root' style={sx} />
+    }
 }
 
 export default LikeButton
