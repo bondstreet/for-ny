2ca2ed79eaf3faf1912f4c3d071d3fc27e593b59
anh-thu huynh <oldwestaction@users.noreply.github.com>
2016-07-05 16:04:32 -0400

    adding masonry layout to posts section (#98)
    * adding masonry layout
    
    * removing newline

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,35 +1,41 @@
 
 import React from 'react'
+import Masonry from 'react-masonry-component'
 import Link from './Link'
 import PostCard from './PostCard'
 import SocialPromo from './SocialPromo'
 
-const PostList = ({ limit }, { data }) => {
-  const posts = limit ? data.posts.slice(0, limit) : data.posts
+class PostList extends React.Component {
+    constructor(props) {
+        super()
+    }
 
-  const postCards = posts.map((post, i) => (
-      <div key={i}
-          className='inline-block align-top col-12 md-col-6 lg-col-4 px2 mb4'>
-          <PostCard {...post} />
-      </div>
-  ))
+    render() {
+        const posts = this.props.limit ? this.context.data.posts.slice(0, this.props.limit) : this.context.data.posts
 
-  postCards.splice(3, 0, (
-      <div key='social-promo' className='inline-block align-top col-12 md-col-6 lg-col-4 px2 sm-px4 md-px2 lg-px2 mb4'>
-          <SocialPromo />
-      </div>
-  ))
+        const postCards = posts.map((post, i) => (
+            <div key={i}
+                className='inline-block align-top col-12 md-col-6 lg-col-4 px2 mb4'>
+                <PostCard {...post} />
+            </div>
+        ))
 
-  return (
-    <div className='mxn2'>
-      {postCards}
-    </div>
-  )
+        postCards.splice(3, 0, (
+            <div key='social-promo' className='inline-block align-top col-12 md-col-6 lg-col-4 px2 sm-px4 md-px2 lg-px2 mb4'>
+                <SocialPromo />
+            </div>
+        ))
+
+        return (
+            <Masonry className='mxn2'>
+                {postCards}
+            </Masonry>
+        )
+    }
 }
 
 PostList.contextTypes = {
-  data: React.PropTypes.object
+    data: React.PropTypes.object
 }
 
 export default PostList
-
