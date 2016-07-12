8f0e86b4b543c2ed860ee12e2bcfd98fc4ada37d
Anh-Thu Huynh <anh-thu@bondstreet.com>
2016-07-12 16:33:45 -0400

    hotfix for fb post share images

--- src/renderHtml.js
+++ src/renderHtml.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === 'development') {
 const Head = ({ title, social, post, ...props }) => {
     const siteUrl = props.domain + props.baseurl
     const metaTitle = (post.title) ? (post.fbTitle || post.title) : (social.fbTitle || title)
-    const metaFbImage = (post.image) ? siteUrl + post.image : siteUrl + social.fbImage
+    const metaFbImage = (post.image) ? post.image : siteUrl + social.fbImage
     const metaTwitterImage = (post.image) ? post.image : siteUrl + social.twitterCardLargeImage
     const metaUrl = siteUrl + props.path
     const metaDescription = post.socialBlurb || social.description
