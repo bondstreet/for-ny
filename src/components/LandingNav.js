7d375be7fc1f495052a06d990fcce51bdeab7052
Anh-Thu Huynh <anh-thu@bondstreet.com>
2016-07-12 13:48:03 -0400

    making sure share button works even w/ number bubble

--- src/components/LandingNav.js
+++ src/components/LandingNav.js
@@ -89,7 +89,7 @@ class LandingNav extends React.Component {
                     scroll
                     offset={-64}
                     children='Contact' />
-                <div className='nav-share-button nowrap inline-block align-middle mx2 md-my1 overflow-hidden'>
+                <div className='nav-share-button nowrap inline-block align-middle overflow-hidden'>
                     <ShareButtons isNav={true} />
                 </div>
             </nav>
